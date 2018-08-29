d59a2bdf5a9310f74ff288a355ac523a91d1bf7f
ajaygunalan <ajaygunalan1995@gmail.com>
2018-08-29 15:46:56 +0530

    restructured properly

--- build/cool400_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ajay/ASIMOV/kdl_ompl_ws/src/cool400_description
-# Build directory: /home/ajay/ASIMOV/kdl_ompl_ws/build/cool400_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
